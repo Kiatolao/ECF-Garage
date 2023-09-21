bd897caba596c0616ce8abf38d60f586b068ff77
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-21 23:10:59 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const app = express();
 // lien avec la base de donnée
 app.use(express.json());
 
+app.get('/test', (req, res) => {
+    res.json('Test de la route');
+    });
+
 app.listen(8000, () => {
   console.log('Server is running on port 3000');
 });
