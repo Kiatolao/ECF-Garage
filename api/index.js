2486d463309a73dfae5756edb4e9b35c22531869
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-22 00:13:37 +0200

    added routes

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import  express  from "express"
+import  authRoutes  from "./routes/auth.js"
+import  postRoutes  from "./routes/posts.js"
+import  userRoutes  from "./routes/users.js"
+
+const app = express();
+
+// lien avec la base de données
+app.use(express.json());
+// utilisation des routes
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
