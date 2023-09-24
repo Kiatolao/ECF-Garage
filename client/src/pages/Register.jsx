473e35c075f009c2390cc4e5896efc236e49ad27
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-24 19:38:38 +0200

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useState } from 'react';
 
 export const Register = () => {
+
+  const [input, setInput] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  // Ici on recupère la valeur courante du state pour la mettre a jour
+  const handleChange = (e) => {
+    setInput(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
   return (
   <>
   <h1>Enregister un nouvel utilisateur</h1>
   <form >
-    <input type="text" placeholder='Utilisateur' />
-    <input type="text" placeholder='Mot de passe' />
+    <input type="text" placeholder='Utilisateur' name='username' onChange={handleChange} />
+    <input type="text" placeholder='Email' name='email' onChange={handleChange}  />
+    <input type="text" placeholder='Mot de passe' name='password' onChange={handleChange}  />
     <button>Enregister</button>
   </form>
   </>
   )
 }
+
