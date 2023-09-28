a1e23d3867f1565a4cb07b739fe0dbbc99005722
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-28 23:26:06 +0200

    fixed route

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,17 @@
+
 export const addPost =  (req, res) => {
     res.json('du controller');
 };
+export const getPosts =  (req, res) => {
+    res.json('du controller');
+};
+export const getPost =  (req, res) => {
+    res.json('du controller');
+};
+
+export const deletePost =  (req, res) => {
+    res.json('du controller');
+}; 
+export const updatePost =  (req, res) => {
+    res.json('du controller');
+}; 
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import  {addPost} from '../controllers/post.js';
+import { getPosts, getPost, addPost, deletePost, updatePost } from '../controllers/post.js';
 
-// import du controller
+
+// creation du router
 const router = express.Router();
 
-router.get('/');
-router.get('/:id');
-router.get('/test');
-router.get('/test');
-router.get('/test');
-router.get('/test');
- 
+// routes pour les posts
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
+
 export default router;
