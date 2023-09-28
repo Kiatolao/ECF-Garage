8fd8b3236a29de620f2185dc0313eec7fa36a619
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-28 15:12:42 +0200

    Hidden menu pour staff users

--- client/src/dashboard/Sidebar.jsx
+++ client/src/dashboard/Sidebar.jsx
@@ -19,32 +19,41 @@ const Sidebar = ({ onSelectMenuItem }) => {
       <div className="mb-4 border-b border-gray-300"></div>
       <ul>
         <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('ajouter-voiture')}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-          <AiFillCar style={{ marginRight: '0.5rem' }} />
-          Ajouter une voiture
-        </div></li>
+          <div className="flex items-center">
+            <AiFillCar className="mr-2" />
+            Ajouter une voiture
+          </div>
+        </li>
         <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('ajouter-temoignage')}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <GoCodeReview style={{ marginRight: '0.5rem' }} />
-          Ajouter un témoignage
-        </div></li>
+          <div className="flex items-center">
+            <GoCodeReview className="mr-2" />
+            Ajouter un témoignage
+          </div>
+        </li>
         <div className="mb-4 border-b border-gray-300"></div>
-        <p>Administration<br/></p>
+        {currentUser && currentUser.role === 'admin' && (
+          <>
         <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('ajouter-employe')}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <AiOutlineTeam style={{ marginRight: '0.5rem' }} />
-          Ajouter un employé
-        </div></li>
+          <div className="flex items-center">
+            <AiOutlineTeam className="mr-2" />
+            Ajouter un employé
+          </div>
+        </li>
         <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('modifier-services')}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <GrServices style={{ marginRight: '0.5rem' }} />
-          Modifier un service
-        </div></li>
-        <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('definir-horaires')}>        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <MdSchedule style={{ marginRight: '0.5rem' }} />
-          Définir les horaires
-        </div></li>
+          <div className="flex items-center">
+            <GrServices className="mr-2" />
+            Modifier un service
+          </div>
+        </li>
+        <li className="mb-2 cursor-pointer" onClick={() => onSelectMenuItem('definir-horaires')}>
+          <div className="flex items-center">
+            <MdSchedule className="mr-2" />
+            Définir les horaires
+          </div>
+        </li>
         <div className="mb-4 border-b border-gray-300"></div>
+        </>
+        )}
       </ul>
     </div>
   );
