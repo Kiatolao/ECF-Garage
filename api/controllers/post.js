2947565a930050295580f7f0d632fec65c8c8418
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-28 23:58:25 +0200

    Update post.js

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,14 +1,23 @@
+import {db} from '../db.js';
 
 export const addPost =  (req, res) => {
     res.json('du controller');
 };
 export const getPosts =  (req, res) => {
-    res.json('du controller');
-};
-export const getPost =  (req, res) => {
-    res.json('du controller');
+    const q = 'SELECT * FROM posts';
+    db.query(q, (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(result);
+    });
 };
 
+export const getPost =  (req, res) => {
+    const q = 'SELECT * FROM posts WHERE id = ?';
+    db.query (q, [req.params.id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(result);
+    });
+};
 export const deletePost =  (req, res) => {
     res.json('du controller');
 }; 
