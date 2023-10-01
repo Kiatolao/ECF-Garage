de983135449a3e13363751954627e6894a3f6e4a
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-01 03:43:42 +0200

    added message server

---
+++ api/controllers/message.js
@@ -0,0 +1,67 @@
+import { db } from '../db.js';
+import jwt from 'jsonwebtoken';
+
+// récupérer tous les messages
+export const getMessages = (req, res) => {
+  const q = 'SELECT * FROM messages';
+  db.query(q, (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json(result);
+  });
+};
+
+//récupérer un message avec son ID
+export const getMessage = (req, res) => {
+  const q = 'SELECT * FROM messages WHERE id = ?';
+  db.query(q, [req.params.id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json(result);
+  });
+};
+
+//supprimer un message avec son ID
+export const deleteMessage = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Vous devez être connecté pour supprimer un message.");
+
+  jwt.verify(token, "jwtkey", (err) => {
+    if (err) return res.status(403).json("Vous n'êtes pas autorisé à supprimer ce message.");
+
+    const messageId = req.params.id;
+    const q = 'DELETE FROM messages WHERE `id` = ?';
+
+    db.query(q, [messageId], (err, data) => {
+      if (err) {
+        return res.status(500).json('Une erreur s\'est produite lors de la suppression du message.');
+      }
+      return res.status(200).json('Le message a été supprimé avec succès.');
+    });
+  });
+};
+
+// Méthode pour ajouter un nouveau message
+export const addMessage = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Pas de token trouvé.");
+
+  jwt.verify(token, "jwtkey", (err) => {
+    if (err) return res.status(403).json("Le token n'est pas valide.");
+
+    const q =
+      "INSERT INTO messages (`firstName`, `lastName`, `email`, `phone`, `message`) VALUES (?)";
+
+    const values = [
+      req.body.firstName,
+      req.body.lastName,
+      req.body.email,
+      req.body.phone,
+      req.body.message,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Le message a été ajouté avec succès.");
+    });
+  });
+};
+
