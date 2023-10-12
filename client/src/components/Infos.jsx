a779348432d2d63118cd3c76a4123b1edf088eb5
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-12 19:01:35 +0200

    Centered info section, modified color font

--- client/src/components/Infos.jsx
+++ client/src/components/Infos.jsx
@@ -10,7 +10,7 @@ export const Infos = () => {
     <section className="text-dak p-4 mt-5">
     <div className="mb-2 mt-2 border-b border-gray-300"></div>  
     <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 mt-5">
-        <div className="md:col-span-1">
+        <div className="md:col-span-1 mx-auto">
             <iframe
               title="map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42423.73197165776!2d-1.2365334806339996!3d48.35123714430663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48093778fd795953%3A0x6531938abaecd607!2s35133%20Foug%C3%A8res%2C%20France!5e0!3m2!1sfr!2sus!4v1697126506785!5m2!1sfr!2sus"
@@ -21,7 +21,7 @@ export const Infos = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
         </div>
-        <div className="md:col-span-1">
+        <div className="md:col-span-1 mx-auto">
             <h3 className="text-xl font-semibold mb-2">
                 <div className="flex items-center">
                     <MdOutlineSchedule className="mr-2" />Horaires
@@ -29,7 +29,7 @@ export const Infos = () => {
             </h3>
           <Schedules />
         </div>
-        <div className="md:col-span-1">
+        <div className="md:col-span-1 mx-auto">
             <h3 className="text-xl font-semibold mb-2">
                 <div className="flex items-center">
                     <MdOutlineContactPhone className="mr-2" />Contact
