c610a75b279411ed383077969714f745bdfab510
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-15 02:14:52 +0200

    added service style

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -5,33 +5,51 @@ import working2 from '../assets/working2.jpg';
 import working5 from '../assets/working5.jpg';
 import working4 from '../assets/working4.jpg';
 import { Services } from '../components/services/Services';
+import { GoGear } from 'react-icons/go';
+import garage from '../assets/back.jpg';
+
 
 export const Overview = () => {
+    const divStyle = {
+      backgroundImage: `url(${garage})`,
+      backgroundAttachment: 'fixed', 
+    };
 
   return (
     <>
-      <Services />
-      <div className=" flex flex-col md:flex-row bg-neutral-200 p-3 shadow-md ">
-        <div class="grid grid-cols-2 gap-1 max-w-[600px] mx-auto pt-1">
+    <div className="p-5">
+      <div className=" flex flex-col md:flex-row pt-10 pb-10 ">
+        <div className="grid grid-cols-2 gap-1 max-w-[600px] mx-auto pt-1">
           <div>
-              <img class="h-auto max-w-full rounded" src={working5} alt=""/>
+              <img className="h-auto max-w-full rounded" src={working5} alt=""/>
           </div>
           <div>
-              <img class="h-auto max-w-full rounded" src={working} alt=""/>
+              <img className="h-auto max-w-full rounded" src={working} alt=""/>
           </div>
           <div>
-              <img class="h-auto max-w-full rounded" src={working2} alt=""/>
+              <img className="h-auto max-w-full rounded" src={working2} alt=""/>
           </div>
           <div>
-              <img class="h-auto max-w-full rounded" src={working4} alt=""/>
+              <img className="h-auto max-w-full rounded" src={working4} alt=""/>
           </div>
       </div>
-      <div className="max-w-[400px] mx-auto">
+      <div className="max-w-[500px] mx-auto pt-4 ">
           <h2 className="text-2xl font-bold mb-5 text-red-600">Un garage à votre service</h2>
           <p className="mb-4 text-justify">
-            <p>Depuis 2004, notre équipe de professionnels qualifiés assure des réparations de confiance et propose une sélection soignée de véhicules d'occasion de qualité. <br/>Nous offrons un service complet, de la réparation à l'achat, en mettant l'accent sur la satisfaction du client.</p>  <br/> Chez Garage Parrot, la fiabilité, l'expertise et l'engagement sont au cœur de notre approche. Faites confiance à notre garage pour prendre soin de votre véhicule et vous guider dans le choix de votre prochaine voiture.
+            Depuis 2004, notre équipe de professionnels qualifiés assure des réparations de confiance et propose une sélection soignée de véhicules d'occasion de qualité. <br />
+            Nous offrons un service complet, de la réparation à l'achat, en mettant l'accent sur la satisfaction du client.
+          </p>
+          <p className="mb-4 text-justify">
+            Chez Garage Parrot, la fiabilité, l'expertise et l'engagement sont au cœur de notre approche. Faites confiance à notre garage pour prendre soin de votre véhicule et vous guider dans le choix de votre prochaine voiture.
           </p>
         <button className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 mb-5 mt-5">Rencontrez-nous <AiOutlineRight size={17} className="inline-block" /></button>
+      </div>
+    </div>
+    </div>
+    <div style={divStyle} className="p-5 bg-no-repeat bg-center bg-cover">
+    <h1 className=" flex justify-center items-center font-bold mt-5 mx-auto text-2xl text-white"><GoGear className="mr-2" /> Nos services</h1>
+      <div className="flex flex-wrap justify-center mb-5 mx-auto">
+        <Services className="" />
       </div>
     </div>
     </>
--- client/src/components/services/Services.jsx
+++ client/src/components/services/Services.jsx
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BiChevronsRight } from 'react-icons/bi';
+
 
 export const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
-      // récuperation des services 
-      async function fetchServices() {
-        try {
-          const response = await axios.get('http://localhost:8000/api/services');
-          setServices(response.data);
-        } catch (error) {
-          console.error('Erreur lors de la récupération des services :', error);
+        // récupération des services 
+        async function fetchServices() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/services');
+                setServices(response.data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des services :', error);
+            }
         }
-      }
-  
-      fetchServices();
+
+        fetchServices();
     }, []);
-  return (
-    <div className="p-5 md:w-1/2 lg:w-1/3 xl:w-1/4">
-    <h2 className="text-xl font-bold mb-4">Nos services</h2>
-    <ul className="list-disc pl-6">
-      {services.map((service) => (
-        <li key={service.id} className="mb-2">{service.service}</li>
-      ))}
-    </ul>
-  </div>
-  )
-}
+
+    return (
+        <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {services.map((service) => (
+            <div key={service.id} className="flex items-center text-lg text-white">
+            <BiChevronsRight size={25} className="" />
+            <span>{service.service}</span>
+            </div>
+        ))}
+        </div>
+    );
+};
 
