96210e5f3aac3e69f706539a6b53caba3c38ad45
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-22 17:09:15 +0200

    fixed bug with <p> hierarchy

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -72,9 +72,9 @@ export const Testimonial = () => {
             className="text-red-600 hover:text-red-700 py-2 px-4 font-semibold cursor-pointer">
             Voir plus de commentaires
           </p>
-          <p>
+
             <TestimonialButton />
-          </p>
+
         </div>
       )}
 </>
