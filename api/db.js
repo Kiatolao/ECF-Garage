81e7d702f2bfa2eaaa77348592006ff0ec81a446
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-25 04:23:13 +0200

    Update db.js

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 
 // Create a connection to the database
 export const db = mysql.createConnection({
-    host: 'sql11.freesqldatabase.com',
+    host: 'jdbc:mysql://sql11.freesqldatabase.com:3306/sql11656434',
     user: 'sql11656434',
     password: '2CjwrHDJjf',
     database: 'sql11656434',   
