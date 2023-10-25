4dff4de3fddd367882f542c9193b0543f0725443
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-25 04:06:43 +0200

    testing db

--- api/db.js
+++ api/db.js
@@ -2,8 +2,8 @@ import mysql from 'mysql';
 
 // Create a connection to the database
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'garage'   
+    host: 'sql11.freesqldatabase.com',
+    user: 'sql11656434',
+    password: '2CjwrHDJjf',
+    database: 'sql11656434',   
 });
