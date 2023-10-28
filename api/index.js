79c9167c88b8536b5b96ecd3e1d6b9dc907a91b6
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-28 20:36:43 +0200

    removed unused upload in index

--- api/index.js
+++ api/index.js
@@ -7,23 +7,12 @@ import  servicesRoutes  from "./routes/services.js";
 import  testimonialsRoutes  from "./routes/testimonials.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import multer from "multer";
 import dotenv from "dotenv";
-import {v2 as cloudinary} from 'cloudinary';
-import streamifier from 'streamifier';
-
 
 dotenv.config();
 
 const apiUrl = process.env.API_URL_SERVER;
 const app = express();
-
-//config cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 app.use((err, req, res, next) => {
   console.error(err.stack); // Log the error
@@ -47,67 +36,6 @@ app.use((req, res, next) => {
 
 // activation de cookie-parser
 app.use(cookieParser());
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
-
-async function handleUpload(file) {
-  const res = await cloudinary.uploader.upload(file, {
-    resource_type: "auto",
-  });
-  return res;
-}
-
-app.post("/api/upload", upload.single("file"), async (req, res) => {
-  try {
-    const b64 = Buffer.from(req.file.buffer).toString("base64");
-    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
-    const cldRes = await handleUpload(dataURI);
-    const imageUrl = cldRes.secure_url;
-    res.json({ imageUrl });
-  } catch (error) {
-    console.log(error);
-    res.send({
-      message: error.message,
-    });
-  }
-});
-
-/* // activation de multer 
-const maxSize = 5 * 1024 * 1024;
-// activation et configuration de multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../client/public/upload/'); 
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  }
-});
-//limitation de la taille du fichier
-const upload = multer({
-  storage: storage,
-  limits: {fileSize: maxSize} 
-});
-
-app.post('/api/upload', upload.single('file'), (req, res) => {
-
-  if(req.file.size > maxSize) {
-    return res.status(400).send('File too large');
-  }
-
-  const file = req.file;
-  res.status(200).json(file.filename);
-}); */
-
-
 
 // utilisation des routes
 app.use("/api/auth", authRoutes);
--- client/src/dashboard/cars/AddCar.jsx
+++ client/src/dashboard/cars/AddCar.jsx
@@ -35,7 +35,7 @@ export const AddCar = () => {
       //setup cloudinary
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", "garageupload");
+      formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
       formData.append("timestamp", (Date.now() / 1000) | 0);
   
       const response = await axios.post("https://api.cloudinary.com/v1_1/doz6ojndh/image/upload", formData, {
