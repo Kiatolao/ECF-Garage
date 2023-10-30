4b8abdf6ec9de237112a399a8d19c92b52ce65a9
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-10-30 19:10:43 +0100

    fixed indexes on splash

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,21 +14,24 @@ export const Home = () => {
   };
   return (
     <>
-    <SocialMedia />
+
 <div style={divStyle} className="p-5 bg-no-repeat bg-center bg-cover h-screen shadow-lg">
-    <div className="absolute inset-0 flex flex-col justify-center items-start  text-white  pointer-events-none">
-    <div className="max-w-[500px] mx-auto mt-20 bg-black bg-opacity-20 p-5 border-black shadow-xl rounded sm:ml-10 ">
+    <div className="absolute inset-0 flex flex-col justify-center items-start  text-white">
+    <div className="max-w-[500px] mx-auto mt-20 bg-black bg-opacity-30 p-10 shadow-xl rounded sm:ml-10 ">
       <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl  ">
         <span className="font-bold">Garage Parrot</span> réparations automobiles de confiance depuis <span className="font-bold">2004</span>
       </h1>
       <p className="mt-5">Nous utilisons des marques de pièces automobiles de haute qualité et nous sommes déterminés à rendre vos trajets sur la route sûrs et confortables.</p>
       
       <Link to="/contact">
-      <button className="bg-red-700 text-white py-2 px-4 mt-8 rounded hover:bg-red-800 shadow-md">
+      <button className="bg-red-700 text-white py-2 px-4 mt-8 rounded hover:bg-red-800 shadow-md z-20 focus:outline-none">
         Demander un service <AiOutlineRight size={17} className="inline-block" />
       </button>
       </Link>
       </div>
+      <div className="fixed right-1 top-1/4 transform -translate-y-1/2 z-30">
+      <SocialMedia />
+  </div>
   </div>
 </div>
     <div>
