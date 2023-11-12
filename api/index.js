9f9a6ac98308f7224629eee4f34e8273623a7545
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-11-12 18:49:30 +0100

    set headers

--- api/index.js
+++ api/index.js
@@ -32,6 +32,9 @@ app.use(cors({
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', `${apiUrl}`);
   next();
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 // activation de cookie-parser
